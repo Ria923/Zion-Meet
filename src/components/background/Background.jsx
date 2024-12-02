32eb0ae8dafc705a7693ee39fec6910c06556037
Ria923 <pudding9311519@gmail.com>
2024-12-02 22:56:11 +0900

    Updated files on Windows

--- src/components/background/Background.jsx
+++ src/components/background/Background.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import styles from"./Background.module.css"; // 正確導入 CSS 模組
+import styles from "./Background.module.css"; // 正確導入 CSS 模組
 
-function GlobalBackground() {
+const GlobalBackground = ({ children, className = "" }) => {
   return (
-    <div className={styles.gridContainer}>
-      <div className={styles.column}></div> {/* 左側區域 */}
-      <div className={styles.column}></div> {/* 中間區域 */}
-      <div className={styles.gradientColumn}></div> {/* 右側漸層背景 */}
+    <div className={`${styles.gridContainer} ${className}`}>
+      {/* 左側區域 */}
+      <div className={styles.column}></div>
+      {/* 中間區域 */}
+      <div className={styles.column}></div>
+      {/* 右側漸層背景 */}
+      <div className={styles.gradientColumn}></div>
+      {/* 插入 children */}
+      <div className={styles.contentContainer}>{children}</div>
     </div>
   );
-}
+};
 
 export default GlobalBackground;
