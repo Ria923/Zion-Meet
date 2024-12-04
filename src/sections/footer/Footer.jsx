bdee49c7044bb6c5ce1c25277224e09ae19a3db5
Ria <ria@RiadeMacBook-Air.local>
2024-12-04 13:34:39 +0900

    Add update of Mac

--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import styles from './footer.module.css'; 
+import React from "react";
+import styles from "./Footer.module.css";
 
 const Footer = () => {
-  return (
-    
-   <>
-   </>
-  );
+  return <></>;
 };
 
 export default Footer;
